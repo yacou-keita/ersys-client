4b0bca837565d3cac62b7df1b2700e3956becaca
Yacou Keita <yacoukeita0212@gmail.com>
2024-05-31 11:19:28 +0000

    add themme

---
+++ lib/core/theme/pallete.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppPallete {
+  static const Color darkBackgroundcolor = Color.fromRGBO(24, 24, 32, 1);
+  static const Color primary = Color(0xFF1C2E82);
+  static const Color errorColor = Colors.redAccent;
+}
---
+++ lib/core/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:ersys_client/core/theme/pallete.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.darkBackgroundcolor
+  );
+  static final lightThemeMode = ThemeData.light();
+}
