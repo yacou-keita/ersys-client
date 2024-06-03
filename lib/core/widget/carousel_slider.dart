a165022e2591632d4105d7c533cd9d1cc2894ab4
Yacou Keita <yacoukeita0212@gmail.com>
2024-06-03 00:13:56 +0000

    feat: carousel slider, this commit improve code and separe responsablité

--- lib/core/widget/carousel_slider.dart
+++ lib/core/widget/carousel_slider.dart
@@ -4,34 +4,33 @@ import 'package:ersys_client/core/data/card_module.dart';
 import 'package:ersys_client/core/theme/pallete.dart';
 import 'package:flutter/material.dart';
 
-int _activePage = 0;
 final PageController _pageController = PageController(initialPage: 0);
-late Timer _timer;
+
 
 class CarouselSlider extends StatefulWidget {
   final List<CardModuleEntity> items;
+  int _activePage = 0;
+  late Timer _timer;
 
-  const CarouselSlider({super.key, required this.items});
+  CarouselSlider({super.key, required this.items});
 
   @override
   State<CarouselSlider> createState() => _CarouselSliderState();
-
 }
 
 class _CarouselSliderState extends State<CarouselSlider> {
-
-   void startTimer() {
-    _timer = Timer.periodic(const Duration(seconds: 3), (timer) {
+  void startTimer() {
+    widget._timer = Timer.periodic(const Duration(seconds: 3), (timer) {
       if (_pageController.page == widget.items.length - 1) {
-      _pageController.animateToPage(0,
+        _pageController.animateToPage(0,
+            duration: const Duration(milliseconds: 500),
+            curve: Curves.easeInOut);
+        return;
+      }
+      _pageController.nextPage(
           duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
-      return;
-    }
-    _pageController.nextPage(
-        duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
     });
   }
-
 
   @override
   void initState() {
@@ -39,7 +38,6 @@ class _CarouselSliderState extends State<CarouselSlider> {
     startTimer();
   }
 
-  
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -51,7 +49,7 @@ class _CarouselSliderState extends State<CarouselSlider> {
               controller: _pageController,
               onPageChanged: (value) {
                 setState(() {
-                  _activePage = value;
+                  widget._activePage = value;
                 });
               },
               itemCount: widget.items.length,
@@ -64,28 +62,9 @@ class _CarouselSliderState extends State<CarouselSlider> {
           bottom: 4,
           left: 0,
           right: 0,
-          child: Container(
-            color: Colors.transparent,
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                    widget.items.length,
-                    (index) => Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 5),
-                          child: GestureDetector(
-                            onTap: () {
-                              _pageController.animateToPage(index,
-                                  duration: const Duration(milliseconds: 300),
-                                  curve: Curves.easeIn);
-                            },
-                            child: CircleAvatar(
-                              backgroundColor: _activePage == index
-                                  ? AppPallete.primary
-                                  : AppPallete.grey,
-                              radius: 4,
-                            ),
-                          ),
-                        ))),
+          child: SliderIndicator(
+            activePage: widget._activePage,
+            itemLength: widget.items.length,
           ),
         )
       ],
@@ -95,12 +74,48 @@ class _CarouselSliderState extends State<CarouselSlider> {
   @override
   void dispose() {
     super.dispose();
-    _timer.cancel();
+    widget._timer.cancel();
   }
-
 }
 
+class SliderIndicator extends StatefulWidget {
+  late final int itemLength;
+  int activePage;
+  SliderIndicator(
+      {super.key, required this.itemLength, required this.activePage});
 
+  @override
+  State<SliderIndicator> createState() => _SliderIndicatorState();
+}
+
+class _SliderIndicatorState extends State<SliderIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.transparent,
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+              widget.itemLength,
+              (index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    child: GestureDetector(
+                      onTap: () {
+                        _pageController.animateToPage(index,
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn);
+                      },
+                      child: CircleAvatar(
+                        backgroundColor: widget.activePage == index
+                            ? AppPallete.primary
+                            : AppPallete.grey,
+                        radius: 4,
+                      ),
+                    ),
+                  ))),
+    );
+  }
+}
 
 class ImagePlacehoder extends StatelessWidget {
   final String imagePath;
