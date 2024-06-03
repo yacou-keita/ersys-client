a2cf36444ff9824e08f38c43ce9820016acd30b1
Yacou Keita <yacoukeita0212@gmail.com>
2024-06-03 16:27:41 +0000

    feat: this commit add flexible layout

--- lib/landing_module/presentation/pages/home_page.dart
+++ lib/landing_module/presentation/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:ersys_client/landing_module/core/data/card_data.dart';
 import 'package:ersys_client/landing_module/core/data/carousel_data.dart';
 import 'package:ersys_client/landing_module/presentation/widgets/card_module_widget.dart';
 import 'package:ersys_client/landing_module/presentation/widgets/carousel_slider/carousel_slider_widget.dart';
+import 'package:ersys_client/shared/core/theme/pallete.dart';
 import 'package:ersys_client/shared/core/widgets/footer.dart';
 import 'package:ersys_client/shared/domain/entities/card_entity.dart';
 import 'package:flutter/material.dart';
@@ -19,21 +20,37 @@ class HomePage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(
-              height: 250,
-              child: CarouselSliderWidget(items: getActiveItem(carouselData))),
-          SizedBox(
-              height: 440,
-              child: ListView.builder(
-                  itemCount: cardData.length,
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  itemBuilder: (BuildContext context, int index) {
-                    return CardModuleWidget(
-                      imageUrl: cardData[index].imageUrl,
-                      title: cardData[index].title,
-                    );
-                  })),
-          const Footer()
+          Expanded(
+            flex: 2,
+            child: CarouselSliderWidget(items: getActiveItem(carouselData)),
+          ),
+          Container(
+              margin: const EdgeInsets.symmetric(vertical: 8),
+              padding: const EdgeInsets.only(bottom: 2),
+              decoration: const BoxDecoration(
+                  border: Border(
+                      bottom:
+                          BorderSide(color: AppPallete.primary, width: 1.0))),
+              child: Text(
+                "services en ligne".toUpperCase(),
+                style: const TextStyle(
+                    color: AppPallete.scondary,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25),
+              )),
+          Expanded(
+            flex: 3,
+            child: ListView.builder(
+                itemCount: cardData.length,
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                itemBuilder: (BuildContext context, int index) {
+                  return CardModuleWidget(
+                    imageUrl: cardData[index].imageUrl,
+                    title: cardData[index].title,
+                  );
+                }),
+          ),
+          const Expanded(flex: 0,child: Footer())
         ],
       ),
     );
